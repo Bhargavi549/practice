dc764078a098dcfd64cdfb55bc25d6b1937786b0
arkavinod <vinod.kumar@badbeat.com>
2023-12-25 21:58:22 +0530

    packge file initialisation

---
+++ day_01/day_01.js
@@ -0,0 +1,8 @@
+// to intialize package.json file with defauts -> npm init -y
+
+// 1. difference between dependencies and devDependencies
+// dependencies: It contains the packages which we used in projcet(dev and prod),
+// dependencies: npm i package_name
+// devDependencies: It contains the packages but which we can use in dev and local only not in PROD.
+// devDependencies --> Need for local development and testing.
+// devDependencies --> npm i package_name --save-dev
