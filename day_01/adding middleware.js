8bd4a5789d58619cfc8704b52e8f86d9d15e637a
bhargavi <bhargavidora222@gmail.com>
2024-11-20 12:16:08 +0530

    middleware added

--- day_01/adding middleware.js
+++ day_01/adding middleware.js
@@ -1,11 +1,35 @@
-// Express: 
-// express is a frame work built in top of NodeJS, 
-// it gives the waid range of features to the applications.
+//middleware : middleware is a like a helper that recives req and res objects to the application
+//client send the request to the server middle ware process request all the conditions are satisfied request send to server through routes ,server gives response
+// routes: specific path or url to the function(endpoint)
 
-const http = require('http');
+
 const express = require('express');
 
-const app = express();
+const app = express()
 
-const server = http.createServer(app);
-server.listen(6000);
+const port = 8555
+
+// middleware condition:
+// app.use((req,res,next)=>{
+//     if(10<20){    // condition is true routes will exicute
+//         next()    // consition false buffering is happended res not came
+//     }
+// })
+
+// assign middle ware to variable:
+
+const firstHandler = ((req,res,next)=>{
+    if(10>20){
+        next()
+    }else {
+        console.log("not allowed") // print on inspect
+    }
+}) // we can apply this middle ware in specific rout
+
+app.get('/home',firstHandler, (req,res)=>{
+    res.send("home page")
+})
+
+app.get('/login', (req,res)=>{
+    res.send("login page")
+})
